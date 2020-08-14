1039c6856d9838fed081bed3492fc8f2da88f47b
wheat <wheat_yuan@163.com>
2020-08-14 14:16:16 +0800

    移动User模型到Models文件夹

--- config/app.php
+++ config/app.php
@@ -174,6 +174,8 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        // IdeHelper
+        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
 
     ],
 
