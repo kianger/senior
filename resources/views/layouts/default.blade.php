eefba3c16d0f7bf879ade50bbda952c7b3762a1c
wheat <wheat_yuan@163.com>
2020-08-14 12:15:51 +0800

    静态文件浏览器缓存问题

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -6,7 +6,8 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title', 'Weibo App') - Laravel 入门教程</title>
-    <link rel="stylesheet" href="/css/app.css">
+    {{--mix() 方法与 webpack.mix.js 文件里的逻辑遥相呼应--}}
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 </head>
 <body>
 
