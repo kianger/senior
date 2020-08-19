342a15ca07e1ae46e1ac2e511d472fd9c34ca604
wheat <wheat_yuan@163.com>
2020-08-19 14:28:53 +0800

    update

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -54,7 +54,7 @@ class UsersController extends Controller
      */
     public function show(User $user)
     {
-        $this->authorize('update', $user);
+        // $this->authorize('update', $user);
         $statuses = $user->statuses()
             ->orderBy('created_at', 'desc')
             ->paginate(10);
