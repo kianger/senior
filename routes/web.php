dc0b24fbecd44343e1d8953268c71ab11e2a4b11
wheat <wheat_yuan@163.com>
2020-08-19 14:01:18 +0800

    关注和粉丝列表页

--- routes/web.php
+++ routes/web.php
@@ -34,3 +34,7 @@ Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('passw
 
 # 微博发布&删除
 Route::resource('statuses', 'StatusesController', ['only' => ['store', 'destroy']]);
+
+# 粉丝
+Route::get('/users/{user}/followings', 'UsersController@followings')->name('users.followings');
+Route::get('/users/{user}/followers', 'UsersController@followers')->name('users.followers');
